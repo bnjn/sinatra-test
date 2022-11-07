48d1f98fc24f37b412237d0c64ba22bdac1f7c2d
Benjamin Johnson <benjamin.johnson@Benjamins-MacBook-Pro.local>
2022-11-07 16:17:33 +0000

    added new route /both

--- sinatra.rb
+++ sinatra.rb
@@ -8,3 +8,7 @@ end
 get '/llama' do
     '<img src=\'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/1920px-Alpaca_%2831562329701%29.jpg\'/>'
 end
+
+get '/both' do
+    return '<h1>Hello world</h1>' + "\n" + '<img src=\'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/1920px-Alpaca_%2831562329701%29.jpg\'/>'
+end
--- spec/sinatra_spec.rb
+++ spec/sinatra_spec.rb
@@ -19,12 +19,18 @@ RSpec.describe 'Hello Llama App' do
     it "says hello" do
         get '/'
         expect(last_response).to be_ok
-        expect(last_response.body).to eq('<h1>Hello world</h1>')
+        expect(last_response.body).to eq('<h1>Hello world</h1>\n<h1>Hello world</h1>')
     end
 
     it "displays image" do
         get '/llama'
         expect(last_response).to be_ok
         expect(last_response.body).to eq('<img src=\'https://upload.wikimedia.org/wikipedia/commons/thumb/d/db/Alpaca_%2831562329701%29.jpg/1920px-Alpaca_%2831562329701%29.jpg\'/>')
+    end
+
+    it 'displays title' do
+        get '/blog'
+        expect(last_response).to be_ok
+        expect(last_response.body)
     end
 end
